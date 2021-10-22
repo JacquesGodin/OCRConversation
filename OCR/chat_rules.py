e40638f6e83b28e0e64662c24905d104c1c84a4c
JacquesGodin <59340400+JacquesGodin@users.noreply.github.com>
2021-10-22 17:57:45 +0100

    Add files via upload

--- OCR/chat_rules.py
+++ OCR/chat_rules.py
@@ -6,7 +6,7 @@ from datetime import date
 today = date.today()
 print("Today's date:", today)
 
-def get_fun_fact(_):
+def get_fun_fact():
     # URL from where we will fetch the data
     url = "https://uselessfacts.jsph.pl/random.json?language=en"
     
@@ -17,18 +17,32 @@ def get_fun_fact(_):
     data = json.loads(response.text)
     
     useless_fact = data["text"]
-
+    
+    return useless_fact
 
 def rules(question):
 
-    question = question.lower()
+    question_t1 = question.lower().replace(" ","")
+    
+    question_t2 = question_t1.splitlines( )
+    question = question_t2[0]
+    
+    
+    print(len(question))
+    print(question)
 
-    if question == "what is the best business school out there?":
-        answer = "NOVA SBE, from Carcavelos to the World!"
-    elif question == "what is DEL's mission?":
-        answer = "To be a digital transformation tool box in order to prepare the future digital transformation leaders"
-    elif question == "what day is today?":
+    #if question == "what is the best business school out there?":
+    #    answer = "NOVA SBE, from Carcavelos to the World!"
+    #elif question == "what is DEL's mission?":
+    #    answer = "To be a digital transformation tool box in order to prepare the future digital transformation leaders"
+    if question == "whatdayistoday?":
         answer = f"Today is {today}"
-    #elif question == "tell me a fun fact":
-    elif question == "the long way home":
+        print(answer)
+    elif question == "tellmeafunfact":
         answer = get_fun_fact()
+        print(answer)
+    
+    else: 
+        answer = "didn't understand"
+    
+    return answer
